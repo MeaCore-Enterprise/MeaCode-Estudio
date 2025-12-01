94c1e727492bd285163a0479e24acca580fdc685
KronoxYT <keylertamayo@gmail.com>
2025-11-30 22:55:58 -0300

    chore(git): dejar de rastrear src-tauri/target* (artefactos de build)

--- src-tauri/target/debug/build/proc-macro2-c567d0b8f35ed6c4/build_script_build-c567d0b8f35ed6c4.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Tomas\Documents\Proyecto\MeaCode-Estudio\src-tauri\target\debug\build\proc-macro2-c567d0b8f35ed6c4\build_script_build-c567d0b8f35ed6c4.d: C:\Users\Tomas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.103\build.rs
-
-C:\Users\Tomas\Documents\Proyecto\MeaCode-Estudio\src-tauri\target\debug\build\proc-macro2-c567d0b8f35ed6c4\build_script_build-c567d0b8f35ed6c4.exe: C:\Users\Tomas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.103\build.rs
-
-C:\Users\Tomas\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.103\build.rs:
